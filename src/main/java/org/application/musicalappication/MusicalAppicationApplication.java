db8a8b0a51efa03081b7bc0a8f51f859fc109d8a
Evgeny135 <trishin2003@gmail.com>
2024-06-25 14:10:16 +0300

    Initial commit

---
+++ src/main/java/org/application/musicalappication/MusicalAppicationApplication.java
@@ -0,0 +1,13 @@
+package org.application.musicalappication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MusicalAppicationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MusicalAppicationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/application/musicalappication/MusicalAppicationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.application.musicalappication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusicalAppicationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
