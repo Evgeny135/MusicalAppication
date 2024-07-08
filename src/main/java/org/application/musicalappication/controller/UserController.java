20dd2943f860adccc87f23891fad651a8c85c090
Svyatoslav <svyatoslav.wot@gmail.com>
2024-07-09 02:37:52 +0300

    отладка фронтенда

--- src/main/java/org/application/musicalappication/controller/UserController.java
+++ src/main/java/org/application/musicalappication/controller/UserController.java
@@ -1,21 +1,15 @@
 package org.application.musicalappication.controller;
 
-import org.application.musicalappication.model.Client;
-import org.application.musicalappication.model.Playlist;
 import org.application.musicalappication.service.ClientService;
 import org.application.musicalappication.service.PlaylistService;
+import org.application.musicalappication.stub.DataStubs;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/user")
@@ -37,10 +31,11 @@ public class UserController {
     // Добавить список музыки пользователя
     @GetMapping("/{id}")
     public String getUserById(@PathVariable("id") long id, Model model){
-        Client client = clientService.getClientById(id);
-        Optional<List<Playlist>> playlists = playlistService.getPlaylistByClient(client.getId());
-        model.addAttribute("client",client);
-        model.addAttribute("playlists", playlists);
+        DataStubs dataStubs = new DataStubs();
+        model.addAttribute("client",dataStubs.client);
+        model.addAttribute("tracks", dataStubs.tracks);
+        model.addAttribute("albums", dataStubs.albums);
+        model.addAttribute("playlists", dataStubs.playlists);
         return "views/userPage";
     }
 }
--- src/main/java/org/application/musicalappication/stub/DataStubs.java
+++ src/main/java/org/application/musicalappication/stub/DataStubs.java
@@ -1,29 +1,91 @@
 package org.application.musicalappication.stub;
 
-import org.application.musicalappication.model.Client;
-import org.application.musicalappication.model.Playlist;
-import org.application.musicalappication.model.Track;
-
 import java.sql.Time;
+import java.util.LinkedList;
 import java.util.List;
 
 public class DataStubs {
-    ClientStub client;
+    public ClientStub client;
+    public List<PlaylistStub> playlists;
+    public List<PlaylistStub> albums;
+    public List<TrackStub> tracks;
+
+    public DataStubs() {
+        this.client = new ClientStub("Шурик");
+        this.playlists = new LinkedList<>();
+        this.playlists.add(new PlaylistStub("Для сна", "Пользовательский плейлист",3));
+        this.playlists.add(new PlaylistStub("Качалка", "Пользовательский плейлист",666));
+        this.playlists.add(new PlaylistStub("TRUE CVLT", "Пользовательский плейлист",0));
+        this.playlists.add(new PlaylistStub("Постпанк", "Пользовательский плейлист",100));
+        this.albums = new LinkedList<>();
+        this.albums.add(new PlaylistStub("Ночная смена", "EP",4));
+        this.albums.add(new PlaylistStub("Орут хаты", "Сингл",1));
+        this.albums.add(new PlaylistStub("Все что я хочу", "Альбом",9));
+        this.tracks = new LinkedList<>();
+        this.tracks.add(new TrackStub("Работа 2х2","Ночная смена",Time.valueOf("00:3:15")));
+        this.tracks.add(new TrackStub("День курка","Ночная смена",Time.valueOf("00:4:10")));
+        this.tracks.add(new TrackStub("Добрый день","Все что я хочу",Time.valueOf("00:3:15")));
+        this.tracks.add(new TrackStub("Под гитару","Все что я хочу",Time.valueOf("00:3:22")));
+        this.tracks.add(new TrackStub("Большие города","Все что я хочу",Time.valueOf("00:3:15")));
+        this.tracks.add(new TrackStub("Холодная вода","Орут хаты",Time.valueOf("00:5:05")));
+    }
+
     public class ClientStub{
-        String username;
-        ClientStub(String name){
-            username = name;
+        public String  name;
+
+        public String getName() {
+            return name;
+        }
+
+        public ClientStub(String name){
+            this.name = name;
         }
     }
     public class TrackStub{
         String title;
         String album;
         Time length;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getAlbum() {
+            return album;
+        }
+
+        public Time getLength() {
+            return length;
+        }
+
+        public TrackStub(String title, String album, Time length) {
+            this.title = title;
+            this.album = album;
+            this.length = length;
+        }
     }
 
     public class PlaylistStub{
         String title;
-        String type;
+        String playlistType;
         int size;
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getPlaylistType() {
+            return playlistType;
+        }
+
+        public int getSize() {
+            return size;
+        }
+
+        public PlaylistStub(String title, String type, int size) {
+            this.title = title;
+            this.playlistType = type;
+            this.size = size;
+        }
     }
 }
